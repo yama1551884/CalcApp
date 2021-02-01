ff41c1b42d6aa08b5ab1cd47c1888f4ba61e96ff
yama1551884 <yama1551884@gmail.com>
2021-02-01 22:41:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/calcapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import com.google.android.material.snackbar.Snackbar;
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        if (editText1.text.isEmpty() || editText2.text.isEmpty()) {
+            Snackbar.make(v!!, "数値を入力してください", Snackbar.LENGTH_LONG).show()
+            return
+        }
+
+        var e: Double = 0.0
+
+        var a = editText1.text.toString()
+        var b = a.toDouble()
+        var c = editText2.text.toString()
+        var d = c.toDouble()
+
+        when (v!!.id) {
+            R.id.button1 -> e = b + d
+            R.id.button2 -> e = b - d
+            R.id.button3 -> e = b * d
+            R.id.button4 -> e = b / d
+        }
+
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("VALUE1", e)
+        startActivity(intent)
+    }
+}
